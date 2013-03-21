7af29c6c7062b7937eafcfe5a2240ec19224bfbc
Xananax <xananax@yelostudio.com>
2013-03-21 09:59:31 +0200

    first commit

--- main.js
+++ main.js
@@ -11,12 +11,62 @@ require.config({
 
 require(["jquery"],function($){
 
-	"use strict";
+	var	elCache = {}
+	,	$w = $(window)
+	,	defaultPage = '#featured'
+	;
 
-	console.log("JS load complete");
+	function getEl(el){
+		if(!elCache.hasOwnProperty(el)){
+			elCache[el] = $(el);
+			if(!elCache[el].length){elCache[el] = false;}
+		}
+		return elCache[el];
+	}
+
+	function inView(){
+		$toLoad.not('processed').filter(function(){
+			var $e = $(this)
+			, th = 0
+			, wt = $w.scrollTop()
+			, wb = wt + $w.height()
+			, et = $e.offset().top
+			, eb = et + $e.height()
+			;
+			return eb >= wt - th && et <= wb + th;
+		}).each(function(){
+			lazyLoadImage($(this));
+		})
+	}
+
+
+	function route(hash){
+		hash = (hash || defaultPage).split('-');
+		var pageId = hash.shift()
+		,	movieId = (hash.shift() || '0')
+		,	page = pageId+'-page'
+		,	movie = pageId+'-' + movieId
+		,	link = pageId+'-link'
+		,	$page = getEl(page)
+		,	$link = getEl(link)
+		;
+		getEl('.page').not($page.addClass('active')).removeClass('active');
+		getEl('#Navigation a').not($link.addClass('active')).removeClass('active');
+	}
+
+	$w.on('hashchange',function(){route(location.hash);})
 
 	$(document).ready(function() {
-		console.log("DOM tree constructed");
+			
+		$("a[href^=#]").on("click", function(evt){
+			evt.preventDefault();
+			var hash = '#'+this.href.split('#').pop()
+			route(hash);
+			history.pushState({}, "", hash);
+		});
+			
+		route(location.hash || null);
+
 	});
 
 });
